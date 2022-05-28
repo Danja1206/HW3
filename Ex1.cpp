bf6fa6f5297cc9ecb515fbbb68db65b32c9821b7
Danja1206 <105879524+Danja1206@users.noreply.github.com>
2022-05-28 15:23:32 +0300

    Add files via upload

---
+++ Ex1.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Figure 
+{
+public:
+    Figure() {}
+    virtual void area() {}
+protected:
+    const double PI = acos(-1.0);
+};
+
+class Parallelogram : public Figure
+{
+public:
+    Parallelogram(double _a, double _b, double _h) : a(_a), b(_b), h(_h) {}
+    ~Parallelogram() {}
+    virtual void area() { S = a * h; cout << S << endl; }
+protected:
+    double a, b, h, S;
+};
+
+class Circle : public Figure
+{
+public:
+    Circle(double _a) : a(_a) {}
+    ~Circle() {}
+    virtual void  area() { S = PI * pow(a, 2); cout << S << endl; }
+private:
+    double a, S;
+};
+
+class Rectangle : public Parallelogram
+{
+public:
+    Rectangle(double _a, double _b, double _h) : Parallelogram(_a, _b, _h) {}
+    ~Rectangle() {}
+    virtual void area() { S = a * b; cout << S << endl; }
+};
+
+class Square : public Parallelogram
+{
+public:
+    Square(double _a, double _b, double _h) : Parallelogram(_a, _b, _h) {}
+    ~Square() {}
+    virtual void area() { S = pow(a, 2); cout << S << endl; }
+};
+
+class Rhombus : public Parallelogram
+{
+public:
+    Rhombus(double _a, double _b, double _h) : Parallelogram(_a, _b, _h) {}
+    ~Rhombus() {}
+    virtual void area() { S = a * h; cout << S << endl; }
+};
+
+int main()
+{
+    Parallelogram P(10,0,5);
+    Circle C(3);
+    Rectangle R(5, 2, 0);
+    Square S(10, 0, 0);
+    Rhombus Rh(11, 0, 5);
+    P.area();
+    C.area();
+    R.area();
+    S.area();
+    Rh.area();
+
+
+    
+}
