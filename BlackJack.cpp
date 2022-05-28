b233ed4ea412e3b0cf402011e6638602aab8e3d8
Danja1206 <105879524+Danja1206@users.noreply.github.com>
2022-05-28 15:21:33 +0300

    Add files via upload

---
+++ BlackJack.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Card
+{
+public:
+    enum suit {
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    };
+    enum value {
+        ACE = 1,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+    };
+    void Flip() 
+    {
+        if (!cardPosition)
+            cardPosition = true;
+        else cardPosition = false;
+    }
+    value getValue()
+    {
+        if (cardPosition)
+        {
+            return v;
+        }
+    }
+private:
+    suit s;
+    value v;
+    bool cardPosition;
+};
+
+int main()
+{
+    Card C;
+    C.Flip();
+    C.getValue();
+}
